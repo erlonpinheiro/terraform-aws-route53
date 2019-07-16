4cac492ff6e03ef63390cf9f7050e53f7f36f2f2
Erlon Pinheiro <erlons@gmail.com>
2019-07-16 12:27:45 -0400

    Adjust index reference on outputs because of the count directive use.

--- outputs.tf
+++ outputs.tf
@@ -2,22 +2,22 @@
 
 output "name_server_for_delegation1" {
   description = "The first nameserver to be configured as subdomain NS record at the domain name."
-  value       = "${aws_route53_zone.new.name_servers.0}"
+  value       = "${aws_route53_zone.new.0.name_servers}"
 }
 
 output "name_server_for_delegation2" {
   description = "The second nameserver to be configured as subdomain NS record at the domain name."
-  value       = "${aws_route53_zone.new.name_servers.1}"
+  value       = "${aws_route53_zone.new.1.name_servers}"
 }
 
 output "name_server_for_delegation3" {
   description = "The third nameserver to be configured as subdomain NS record at the domain name."
-  value       = "${aws_route53_zone.new.name_servers.2}"
+  value       = "${aws_route53_zone.new.2.name_servers}"
 }
 
 output "name_server_for_delegation4" {
   description = "The fourth nameserver to be configured as subdomain NS record at the domain name."
-  value       = "${aws_route53_zone.new.name_servers.3}"
+  value       = "${aws_route53_zone.new.3.name_servers}"
 }
 
 output "dns_record" {
