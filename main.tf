9aa003675a7897e1536153c54a327968bd6e72ed
Erlon Pinheiro <erlons@gmail.com>
2019-07-16 12:03:26 -0400

    Remove duplicated aws_route53_zone resource.

--- main.tf
+++ main.tf
@@ -10,19 +10,8 @@ resource "aws_route53_zone" "new" {
     )}"
 }
 
-
-resource "aws_route53_zone" "new" {
-  count   = "${var.public_hosted_zone != "" ? 1 : 0}"
-  name    = "${var.public_hosted_zone}"
-  comment = "${var.comment}"
-
-  tags = "${merge(
-      var.tags
-    )}"
-}
-
 resource "aws_route53_record" "new" {
-  count   = "${var.public_hosted_zone != "" ? 1 : 0}"
+  count   = "${var.zone_record != "" ? 1 : 0}"
   zone_id = "${var.zone_id}"
   name    = "${var.zone_name}"
   type    = "${var.zone_type}"
